2e7204e585126bc899868064f8df01454975221e
한규태 <trapkka997@gmail.com>
2018-08-13 13:38:35 +0900

    test3

--- 5Sake_Cocktail/src/main/java/sesoc/global/cocktail/controller/HomeController.java
+++ 5Sake_Cocktail/src/main/java/sesoc/global/cocktail/controller/HomeController.java
@@ -1,7 +1,13 @@
 package sesoc.global.cocktail.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import org.slf4j.Logger;
@@ -10,10 +16,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * Handles requests for the application home page.
- */
+import com.google.cloud.vision.v1.AnnotateImageRequest;
+import com.google.cloud.vision.v1.AnnotateImageResponse;
+import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
+import com.google.cloud.vision.v1.EntityAnnotation;
+import com.google.cloud.vision.v1.Feature;
+import com.google.cloud.vision.v1.Image;
+import com.google.cloud.vision.v1.ImageAnnotatorClient;
+import com.google.cloud.vision.v1.Feature.Type;
+import com.google.protobuf.ByteString;
+
 @Controller
 public class HomeController {
 	
@@ -31,6 +45,47 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
+	}
+	@RequestMapping(value ="/test", method=RequestMethod.GET)
+	public @ResponseBody String test(Locale locale, Model model) throws IOException {
+		 // Instantiates a client
+		 try (ImageAnnotatorClient vision = ImageAnnotatorClient.create()) {
+
+		   // The path to the image file to annotate
+		   String fileName = "/Users/hangyutae/Downloads/4k-wallpaper-berries-delicious-1266741.jpg";
+
+		   // Reads the image file into memory
+		   Path path = Paths.get(fileName);
+		   byte[] data = Files.readAllBytes(path);
+		   ByteString imgBytes = ByteString.copyFrom(data);
+
+		   // Builds the image annotation request
+		   List<AnnotateImageRequest> requests = new ArrayList<>();
+		   Image img = Image.newBuilder().setContent(imgBytes).build();
+		   Feature feat = Feature.newBuilder().setType(Type.LABEL_DETECTION).build();
+		   AnnotateImageRequest request = AnnotateImageRequest.newBuilder()
+		       .addFeatures(feat)
+		       .setImage(img)
+		       .build();
+		   requests.add(request);
+
+		   // Performs label detection on the image file
+		   BatchAnnotateImagesResponse response = vision.batchAnnotateImages(requests);
+		   List<AnnotateImageResponse> responses = response.getResponsesList();
+
+		   for (AnnotateImageResponse res : responses) {
+		     if (res.hasError()) {
+		       System.out.printf("Error: %s\n", res.getError().getMessage());
+		       return "Error";
+		     }
+
+		     for (EntityAnnotation annotation : res.getLabelAnnotationsList()) {
+		       annotation.getAllFields().forEach((k, v) ->
+		           System.out.printf("%s : %s\n", k, v.toString()));
+		     }
+		   }
+		 }
+		 return "tt";
 	}
 	
 }
