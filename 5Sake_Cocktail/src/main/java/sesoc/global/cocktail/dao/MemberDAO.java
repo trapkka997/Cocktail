b714e66128341d3ee2ade564f9e6cab6bf28bf73
user <user@user-PC>
2018-08-16 15:35:49 +0900

    dao, membercontroller 추가

--- 5Sake_Cocktail/src/main/java/sesoc/global/cocktail/dao/MemberDAO.java
+++ 5Sake_Cocktail/src/main/java/sesoc/global/cocktail/dao/MemberDAO.java
@@ -1,5 +1,13 @@
 package sesoc.global.cocktail.dao;
 
+import java.util.List;
+
+import sesoc.global.cocktail.vo.User;
+
 public interface MemberDAO {
 
+	public int insertMember(User vo); 
+	public User selectOne(User vo);
+	public List<User> selectAll();
+	
 }
