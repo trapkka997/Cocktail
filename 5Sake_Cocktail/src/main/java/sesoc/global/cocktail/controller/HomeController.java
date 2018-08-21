e7aeca464c2a8f00b85fce4a813c2db08a6406d8
ㅎㅣ흫 <@10.10.12.53>
2018-08-21 18:59:14 +0900

    dd

--- 5Sake_Cocktail/src/main/java/sesoc/global/cocktail/controller/HomeController.java
+++ 5Sake_Cocktail/src/main/java/sesoc/global/cocktail/controller/HomeController.java
@@ -1,39 +1,15 @@
 package sesoc.global.cocktail.controller;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.text.DateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.Locale;
-import java.util.Set;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
-
-import com.google.cloud.vision.v1.AnnotateImageRequest;
-import com.google.cloud.vision.v1.AnnotateImageResponse;
-import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
-import com.google.cloud.vision.v1.EntityAnnotation;
-import com.google.cloud.vision.v1.Feature;
-import com.google.cloud.vision.v1.Image;
-import com.google.cloud.vision.v1.ImageAnnotatorClient;
-import com.google.cloud.vision.v1.Feature.Type;
-import com.google.protobuf.ByteString;
 
 @Controller
 public class HomeController {
@@ -68,7 +44,7 @@ public class HomeController {
 	}
 	@RequestMapping(value = "/newC", method = RequestMethod.GET)
 	public String newC(Locale locale, Model model) {
-		return "NewFile";
+		return "login";
 	}
 	
 	
