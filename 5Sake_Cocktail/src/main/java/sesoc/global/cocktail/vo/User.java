69b78f189e0761f24c32ae9fed2bc06b524e07b6
jayca <jayca@DESKTOP-OUQ166D>
2018-08-28 20:15:44 +0900

    uservo

--- 5Sake_Cocktail/src/main/java/sesoc/global/cocktail/vo/User.java
+++ 5Sake_Cocktail/src/main/java/sesoc/global/cocktail/vo/User.java
@@ -1,90 +1,40 @@
 package sesoc.global.cocktail.vo;
 
 public class User {
-<<<<<<< HEAD
-	private String useremail; 
-	private String usernickname;
-	private String userpwd;
-=======
 	private String userEmail;
 	private String userPwd;
 	private String userNickname;
 	private String userAuthCode;
 	private String userAuth;
->>>>>>> branch 'master' of https://github.com/trapkka997/Cocktail
 	public User() {
 		super();
 	}
-<<<<<<< HEAD
-	public User(String useremail, String usernickname, String userpwd) {
-=======
 	public User(String userEmail, String userPwd, String userNickname, String userAuthCode, String userAuth) {
->>>>>>> branch 'master' of https://github.com/trapkka997/Cocktail
 		super();
-<<<<<<< HEAD
-		this.useremail = useremail;
-		this.usernickname = usernickname;
-		this.userpwd = userpwd;
-=======
 		this.userEmail = userEmail;
 		this.userPwd = userPwd;
 		this.userNickname = userNickname;
 		this.userAuthCode = userAuthCode;
 		this.userAuth = userAuth;
->>>>>>> branch 'master' of https://github.com/trapkka997/Cocktail
 	}
-<<<<<<< HEAD
-	
-	public String getUseremail() {
-		return useremail;
-=======
 	public String getUserEmail() {
 		return userEmail;
->>>>>>> branch 'master' of https://github.com/trapkka997/Cocktail
 	}
-<<<<<<< HEAD
-	public void setUseremail(String useremail) {
-		this.useremail = useremail;
-=======
 	public void setUserEmail(String userEmail) {
 		this.userEmail = userEmail;
->>>>>>> branch 'master' of https://github.com/trapkka997/Cocktail
 	}
-<<<<<<< HEAD
-	public String getUsernickname() {
-		return usernickname;
-=======
 	public String getUserPwd() {
 		return userPwd;
->>>>>>> branch 'master' of https://github.com/trapkka997/Cocktail
 	}
-<<<<<<< HEAD
-	public void setUsernickname(String usernickname) {
-		this.usernickname = usernickname;
-=======
 	public void setUserPwd(String userPwd) {
 		this.userPwd = userPwd;
->>>>>>> branch 'master' of https://github.com/trapkka997/Cocktail
 	}
-<<<<<<< HEAD
-	public String getUserpwd() {
-		return userpwd;
-=======
 	public String getUserNickname() {
 		return userNickname;
->>>>>>> branch 'master' of https://github.com/trapkka997/Cocktail
 	}
-<<<<<<< HEAD
-	public void setUserpwd(String userpwd) {
-		this.userpwd = userpwd;
-=======
 	public void setUserNickname(String userNickname) {
 		this.userNickname = userNickname;
->>>>>>> branch 'master' of https://github.com/trapkka997/Cocktail
 	}
-<<<<<<< HEAD
-	
-=======
 	public String getUserAuthCode() {
 		return userAuthCode;
 	}
@@ -97,20 +47,10 @@ public class User {
 	public void setUserAuth(String userAuth) {
 		this.userAuth = userAuth;
 	}
->>>>>>> branch 'master' of https://github.com/trapkka997/Cocktail
 	@Override
 	public String toString() {
-<<<<<<< HEAD
-		return "User [useremail=" + useremail + ", usernickname=" + usernickname + ", userpwd=" + userpwd + "]";
-=======
 		return "User [userEmail=" + userEmail + ", userPwd=" + userPwd + ", userNickname=" + userNickname
 				+ ", userAuthCode=" + userAuthCode + ", userAuth=" + userAuth + "]";
->>>>>>> branch 'master' of https://github.com/trapkka997/Cocktail
 	}
-<<<<<<< HEAD
-=======
-	
 
-	
->>>>>>> branch 'master' of https://github.com/trapkka997/Cocktail
 }
